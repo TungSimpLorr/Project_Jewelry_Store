2c3d3412e7ee3ed1e5e36f5b7bbd62b01548aaec
TungSimpLorr <tung5f2l@gmail.com>
2025-06-12 21:36:43 +0700

    tung cap nhat code 12/6

--- include.js
+++ include.js
@@ -1,4 +1,3 @@
-
 function loadHTML(selector, filePath) {
     fetch(filePath)
         .then(response => {
@@ -10,3 +9,46 @@ function loadHTML(selector, filePath) {
         })
         .catch(error => console.error(error));
 }
+
+
+
+function hienkhoidanhmuc() {
+    const khoidanhmuc = document.getElementsByClassName('new-menu-bar')[0];
+    if (khoidanhmuc) {
+        if (khoidanhmuc.style.display === 'none' || khoidanhmuc.style.display === '') {
+            khoidanhmuc.style.display = 'block';
+        } else {
+            khoidanhmuc.style.display = 'none';
+        }
+    }
+}
+
+function changebanner() {
+    const banner = document.getElementsByClassName('boxbanner')[0];
+    const sidebar = document.getElementsByClassName('new-menu-bar')[0];
+    sidebarnow = sidebar.style.display;
+
+    if (sidebarnow) {
+        if (sidebarnow.style.display === 'none') {
+            banner.style.width = '100%';
+        } else {
+            banner.style.width = '70%';
+        }
+    }
+}
+
+// mo danh muc san pham
+$(document).ready(function() {
+
+
+    // an danh muc san pham 
+    $('.box-content-new-bar').hide();
+   
+    $('#cart-list-icon').click(function(e){
+        e.preventDefault();
+        $('.box-content-new-bar').slideToggle(300);
+    });
+    
+
+});
+
